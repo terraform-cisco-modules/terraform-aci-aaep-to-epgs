8417a54bc1c2c54cea656d92ce04579c5484468e
scotttyso <tyscott@cisco.com>
2024-02-24 08:57:49 -0500

    minor fix for aaep mappings

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+/*_____________________________________________________________________________________________________________________
+
+AAEP to EPG — Outputs
+_______________________________________________________________________________________________________________________
+*/
 output "aaep_to_epgs" {
   description = "Map of AAEP to EPG Settings."
   value       = { for k in sort(keys(aci_epgs_using_function.epg_to_aaeps)) : k => aci_epgs_using_function.epg_to_aaeps[k].id }
